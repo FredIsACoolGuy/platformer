e999a2a157a7b376e09f302e851c45258162a448
Fred <duffielf@sheridancollege.ca>
2021-03-22 16:41:25 -0400

    initial commit

---
+++ platformer/Assets/Scripts/Mechanics/SecretZone.cs
@@ -0,0 +1,17 @@
+﻿using Platformer.Mechanics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretZone : MonoBehaviour
+{
+    bool _found;
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        var p = collider.gameObject.GetComponent<PlayerController>();
+        if (p != null && !_found) {
+            Counters.Increment(Counters.CounterType.SecretFind);
+            _found = true;
+        }
+    }
+}
---
+++ platformer/Assets/Scripts/UI/VolumeSlider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    private Slider volSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volSlider = GetComponent<Slider>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioListener.volume = volSlider.value;
+    }
+}
